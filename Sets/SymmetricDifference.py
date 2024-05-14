adc5dc73c097c245385b9d18d2094cef7570c7e3
Nishant Sinha <nishantsinha.hulk@gmail.com>
2024-05-14 15:50:40 +0530

    Hackerrank Solutions

---
+++ Sets/SymmetricDifference.py
@@ -0,0 +1,27 @@
+#   Given 2 sets of integers, M and N, print their symmetric difference in ascending order.
+#   The term symmetric difference indicates those values that exist in either M or N but do not exist in both.
+
+#   Sample Input
+
+#   STDIN       Function
+#   -----       --------
+#   4           set a size M = 4
+#   2 4 5 9     a = {2, 4, 5, 9}
+#   4           set b size N = 4
+#   2 4 11 12   b = {2, 4, 11, 12}
+
+#   Sample Output
+#   5
+#   9
+#   11
+#   12
+
+m = int(input())
+a = set(map(int,input().split()))
+n = int(input())
+b = set(map(int,input().split()))
+
+result = sorted(a.symmetric_difference(b))
+
+for num in result:
+    print(num)
